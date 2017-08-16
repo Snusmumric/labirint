ffb620df1930c4908438679faeeb299203fc2bb0
Ruslan <toctep944@gmail.com>
2017-08-16 22:10:36 +0300

    html sceleton 2

--- server/src/main.go
+++ server/src/main.go
@@ -11,22 +11,6 @@ import (
 
 const (
 	html_dir = "server/html/"
-	pageTop    = `<!DOCTYPE HTML><html align="center"><head>
-<style>.error{color:#FF0000;}</style></head><title>Statistics</title>
-<body><h3>Statistics</h3>
-<p>Computes basic statistics for a given list of numbers</p>`
-	form       = `<form action="/" method="POST">
-<label for="numbers">Numbers (comma or space-separated):</label><br />
-
-<table border="1" align="center">
-<tr><td><a>Your numbers: </a></td><td><input type="text" name="numbers" size="30"></td></tr>
-<tr><td><a>Answer: </a></td><td><input type="text" name="text answer" size="30" value="answer"></td></tr>
-</table>
-
-<p><input type="submit" value="Calculate"></p>
-
-</form>`
-	pageBottom = `</body></html>`
 	anError    = `<p class="error">%s</p>`
 )
 
