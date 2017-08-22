fcf54a297b6fd0ce20a5b961becc9ae65a51a308
Dmitriy Valetov <dmitvalet@gmail.com>
2017-08-22 19:17:32 +0300

    Update workingPoolMk1.go

--- server/src/workingPoolTesting/workingPoolMk1.go
+++ server/src/workingPoolTesting/workingPoolMk1.go
@@ -1,22 +1,22 @@
-package workers
+package workPool
 
 import (
 	"time"
+	"fmt"
 )
 
 type Task interface {
 	Handle() error
-	Finish(error)
+	//Finish(error)
 }
-
 
 type WorkerPool struct {
 	capacity int
 	taskChan chan Task
 }
 
-func NewPool(capacity int) *WorkerPool{
-	return &WorkerPool{ capacity: capacity, taskChan: make(chan Task, capacity)}
+func NewPool(capacity int) *WorkerPool {
+	return &WorkerPool{capacity: capacity, taskChan: make(chan Task)}
 }
 
 func (p *WorkerPool) Run() {
@@ -30,24 +30,27 @@ func (p *WorkerPool) Stop() {
 }
 
 func (p *WorkerPool) AddTaskAsynk(task Task, timeout time.Duration) error {
-	//tick := time.Tick(timeout * time.Millisecond)
-	p.taskChan <- task
-	return nil
-	/*
+
+	//p.taskChan <- task ; return nil
+
+
+	tick := time.Tick(timeout * time.Microsecond)
 	select {
 	case p.taskChan <- task:
 		return nil
 	case <-tick:
+		// в случае, если воркеров не хватает, они заняты,
+		// то через некоторое время таск будет отклонен.
+		// типо попробуйте позднее
+		fmt.Println("Task not sended, timeout")
 		return fmt.Errorf("Task not sended, timeout")
 	}
-	*/
-
 
 }
 
 func (p *WorkerPool) startWorker() {
 	for t := range p.taskChan {
-		err := t.Handle()
-		t.Finish(err)
+		_ = t.Handle()
+		//t.Finish(err)
 	}
 }
