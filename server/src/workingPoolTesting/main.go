d7d39d82fd81a0997a85243d4a5b97b2cbb40fda
Dmitriy Valetov <dmitvalet@gmail.com>
2017-08-22 19:16:27 +0300

    Update main.go

--- server/src/workingPoolTesting/main.go
+++ server/src/workingPoolTesting/main.go
@@ -1,34 +1,51 @@
 package main
 
 import (
-	"./workers"
+	"./workPool"
 	"fmt"
+	"time"
 )
 
-
-type mytask struct {
+type myTask struct {
 	str string
 }
 
-func (t mytask) Handle() error {
-	fmt.Println("Done", t.str)
+func (t myTask) Handle() error {
+	time.Sleep(1*time.Second)
+	fmt.Println(t.str)
+	done<- struct{}{}
 	return nil
 }
 
-func (t mytask) Finish(err error) {
-	fmt.Println(err)
-}
+	var done = make(chan struct{})
 
 func main() {
-	wp := workers.NewPool(10)
 
+	wp := workPool.NewPool(2) // number of workers rules as threads
+	wp.Run()
 
 	defer wp.Stop()
 
-	tsk := mytask{"It's like a piece of cake."}
+	t1 := myTask{"Hello"}
+	t2 := myTask{", my dear"}
+	t3 := myTask{" fellow."}
 
-	wp.AddTaskAsynk(tsk,1)
+	taskList := []myTask{t1,t2,t3}
 
-	wp.Run()
+	for _, t := range taskList {
+		go func(t myTask) {
+			_ = wp.AddTaskAsynk(t, 1)
+			/*
+			if err != nil {
+				fmt.Println(err)
+				wp.Stop()
+				return
+			}
+			*/
+		}(t)
+	}
 
+	for i:=0; i<3; i++{
+		<-done
+	}
 }
