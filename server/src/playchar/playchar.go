946f845eebb63bf0d9f6568a591757e54085df9a
Dmitriy Valetov <dmitvalet@gmail.com>
2017-08-25 22:58:26 +0300

    Update playchar.go

--- server/src/playchar/playchar.go
+++ server/src/playchar/playchar.go
@@ -1,5 +1,7 @@
 package playchar
 
+import "fmt"
+
 type position struct {
 	Posx int
 	Posy int
@@ -10,6 +12,12 @@ type Playchar struct {
 	//position *cell //or i,j int
 	// address of cell may not survive save-load process
 	Position position
+}
+
+func (pl *Playchar)ToString() string{
+	var resultstr string
+	resultstr = fmt.Sprintf("'%d:%d:%d'",pl.Healthpoints,pl.Position.Posx, pl.Position.Posy)
+	return resultstr
 }
 
 func New(hp int, x0 int, y0 int) *Playchar {
