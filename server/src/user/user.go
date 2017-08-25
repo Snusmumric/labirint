d24d6a5b736476f3a8affccbd1c3ed07670f6e94
Dmitriy Valetov <dmitvalet@gmail.com>
2017-08-25 22:57:46 +0300

    Update user.go

--- server/src/user/user.go
+++ server/src/user/user.go
@@ -6,6 +6,8 @@ import (
 	"game"
 	"net/http"
 	"reporter"
+	"strings"
+	"strconv"
 )
 
 type User struct {
@@ -24,7 +26,7 @@ type GameStarter struct {
 }
 
 func (gs *GameStarter) Handle() error {
-	game, err := game.MakeAGame(gs.MapSize, gs.DataBase)
+	game, err := game.MakeAGame(gs.MapSize, "game1", gs.DataBase)
 	if err != nil {
 		return fmt.Errorf("GameStarter: failed to make a game: %s", err)
 	}
@@ -49,7 +51,7 @@ func (gs *GameStarter) Finish(err error) {
 
 func (u *User) CheckActiveGame(dbc *db_client.DBClient) (bool, error) {
 	var count int
-	rows, err := dbc.DB.Query("SELECT status FROM games g INNER JOIN users u ON g.id = ANY(u.games) WHERE u.id=?", u.Id)
+	rows, err := dbc.DB.Query("SELECT status FROM games g INNER JOIN users u ON g.id = ANY(u.games) WHERE u.id=$1", u.Id)
 	if err != nil {
 		return false, err
 	}
@@ -75,15 +77,40 @@ func (u *User) CheckActiveGame(dbc *db_client.DBClient) (bool, error) {
 }
 
 func GetUserById(dbc *db_client.DBClient, id int) (*User, error) {
-	row, err := dbc.DB.Query("SELECT * FROM users WHERE id=?", id)
-	defer row.Close()
+	row, err := dbc.DB.Query("SELECT * FROM users WHERE id=$1", id)
+	fmt.Println(err)
 	if err != nil {
 		return nil, err
 	}
+	defer row.Close()
 	var user User
-	err = row.Scan(&user.Id, &user.Name, &user.Games)
+	row.Next()
+
+	var uint8Games []uint8
+	err = row.Scan(&user.Id, &user.Name, &uint8Games)
+	str := fmt.Sprintf("%s", uint8Games)
+	str = str[1:len(str)-1]
+	NumList := strings.Split(str, ",")
+	for _, s := range NumList {
+		j,_ := strconv.Atoi(s)
+		user.Games = append(user.Games,j)
+	}
+
 	if err != nil {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (us *User)UserHaveTheGameWithId(gameId int) (bool, error) {
+	exists := false
+
+	for _, g := range us.Games {
+		if g == gameId {
+			exists = true
+			return exists, nil
+		}
+	}
+
+	return exists, nil
 }
